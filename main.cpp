3e8e0a35de61acadd045800855077f6c7efed9b9
GuidoHoltkamp <guido@worklaptop>
2016-11-10 21:57:12 +0100

    testcommit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+//#include <boost/lambda/lambda.hpp>
+
+using namespace std;
+
+int count_x(char* p, char x)
+// count the number of occurrences of x in p[]
+// p is assumed to point to a zero-ter minated array of char (or to nothing)
+{
+	if (p == nullptr) return 0;
+	int count = 0;
+	for (; p != nullptr; ++p)
+		if (*p == x)
+			++count;
+	return count;
+}
+
+int main() {
+
+	std::cout << "OMG";
+	std::cin.get();
+
+	return 0;
+
+}
